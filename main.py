2ce6f364d8c155eb862b86cefc87ac73a376f26d
DariuszPakulski2137 <89033390+DariuszPakulski2137@users.noreply.github.com>
2024-03-13 13:47:49 +0100

    proxies usage

--- main.py
+++ main.py
@@ -45,6 +45,8 @@ def getAllTokenNames():
 
 
 async def fetch_token_price(token: Token, semaphore, _id):
+    await semaphore.acquire()
+
     url = str(f"https://api.binance.com/api/v3/uiKlines?symbol={token.symbol}USDT&interval=1m&limit=1")
     connector = ProxyConnector.from_url(proxy)
     session = aiohttp.ClientSession(connector=connector)
@@ -78,7 +80,7 @@ async def fetch_token_price(token: Token, semaphore, _id):
             print("Problem with URL: " + url)
             traceback.print_exc()
 
-    #semaphore.release()
+    semaphore.release()
 
 
 async def fetch_all_token_prices(_tokens):
