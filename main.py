c3eb973b83addcb9a7603fbc7eb226000162f79a
DariuszPakulski2137 <89033390+DariuszPakulski2137@users.noreply.github.com>
2024-03-13 12:15:31 +0100

    proxies usage

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import decouple
 import requests
 import uvicorn
 from fastapi import FastAPI
+from urllib3.contrib.socks import SOCKSHTTPSConnection
 
 from tokens import Token
 
@@ -84,6 +85,8 @@ async def fetch_token_price(token: Token, semaphore, _id):
             _data = json.loads(_data.text)[0]
             price_1h_ago = float(_data[4])
             """
+    except SOCKSHTTPSConnection:
+        pass
     except:
         print("Problem with URL: " + url)
         traceback.print_exc()
