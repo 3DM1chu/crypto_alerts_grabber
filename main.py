bc47d61f058cf91d7679f713209cd44f070088e5
DariuszPakulski2137 <89033390+DariuszPakulski2137@users.noreply.github.com>
2024-03-13 12:57:37 +0100

    proxies usage

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import asyncio
 import multiprocessing
 import random
-import time
 import traceback
-from datetime import datetime, timedelta
+from datetime import datetime
 import json
 from multiprocessing import Process
 from typing import List
@@ -12,7 +11,7 @@ import decouple
 import requests
 import uvicorn
 from fastapi import FastAPI
-from urllib3.contrib.socks import SOCKSHTTPSConnection
+from aiohttp_socks import ProxyType, ProxyConnector, ChainProxyConnector
 
 from tokens import Token
 
@@ -44,50 +43,51 @@ def getAllTokenNames():
     return [token.symbol for token in tokens]
 
 
-async def fetch_token_price(session, token: Token, semaphore, _id):
+async def fetch_token_price(token: Token, semaphore, _id):
     await semaphore.acquire()
 
     urls = [
         f"https://api.binance.com/api/v3/uiKlines?symbol={token.symbol}USDT&interval=1m&limit=1",
         f"https://pexljc3fiphfkworlrtv52mi2q0cqhke.lambda-url.eu-central-1.on.aws/?coin={token.symbol}USDT",
-        #f"https://cold-condor-42.deno.dev/{token.symbol}USDT"
+        # f"https://cold-condor-42.deno.dev/{token.symbol}USDT"
     ]
 
     url = urls[_id % len(urls)]
     if 'api.binance' in url:
-        proxy_use = proxy
+        connector = ProxyConnector.from_url(proxy)
+        session = aiohttp.ClientSession(connector)
     else:
-        proxy_use = None
         await asyncio.sleep(random.randint(1, 5))
-    try:
-        async with session.get(url, proxy=proxy_use) as resp:
-            data = resp.text()
-            data = json.loads(data)
-            token_data = data[0]
-            current_price = float(token_data[4])
-            token.addPriceEntry(current_price, datetime.now())
-            data_to_send = {"symbol": token.symbol, "current_price": token.getCurrentPrice(),
-                            "current_time": token.getCurrentPriceDatetime().strftime("%Y-%m-%d %H:%M:%S")}
-            requests.post(f"{URL_OF_COORDINATOR}/addTokenPrice", data=json.dumps(data_to_send))
-            print(f"Sent token {token.symbol}")
-            """
-            # Get the current time
-            current_time = datetime.now()
-
-            # Subtract 1 hour from the current time
-            adjusted_time = current_time - timedelta(hours=1)
-
-            # Convert adjusted time to Unix time
-            adjusted_unix_time = int(adjusted_time.timestamp()) * 1000
-
-            _data = requests.get(f"https://api.binance.com/api/v3/uiKlines?symbol={token.symbol}USDT&interval=1m"
-                                 f"&limit=1&startTime={adjusted_unix_time}")
-            _data = json.loads(_data.text)[0]
-            price_1h_ago = float(_data[4])
-            """
-    except:
-        print("Problem with URL: " + url)
-        traceback.print_exc()
+        session = aiohttp.ClientSession()
+    async with session:
+        try:
+            async with session.get(url) as resp:
+                data = await resp.json()
+                token_data = data[0]
+                current_price = float(token_data[4])
+                token.addPriceEntry(current_price, datetime.now())
+                data_to_send = {"symbol": token.symbol, "current_price": token.getCurrentPrice(),
+                                "current_time": token.getCurrentPriceDatetime().strftime("%Y-%m-%d %H:%M:%S")}
+                requests.post(f"{URL_OF_COORDINATOR}/addTokenPrice", data=json.dumps(data_to_send))
+                print(f"Sent token {token.symbol}")
+                """
+                # Get the current time
+                current_time = datetime.now()
+    
+                # Subtract 1 hour from the current time
+                adjusted_time = current_time - timedelta(hours=1)
+    
+                # Convert adjusted time to Unix time
+                adjusted_unix_time = int(adjusted_time.timestamp()) * 1000
+    
+                _data = requests.get(f"https://api.binance.com/api/v3/uiKlines?symbol={token.symbol}USDT&interval=1m"
+                                     f"&limit=1&startTime={adjusted_unix_time}")
+                _data = json.loads(_data.text)[0]
+                price_1h_ago = float(_data[4])
+                """
+        except:
+            print("Problem with URL: " + url)
+            traceback.print_exc()
 
     semaphore.release()
 
@@ -95,15 +95,14 @@ async def fetch_token_price(session, token: Token, semaphore, _id):
 async def fetch_all_token_prices(_tokens):
     semaphore = asyncio.Semaphore(HTTP_THREADS)  # Limiting to 10 concurrent requests
     task_id = 0
-    async with aiohttp.ClientSession() as session:
-        while True:  # Run indefinitely
-            if semaphore.locked():
-                await asyncio.sleep(2)
-            async with semaphore:
-                tasks = [fetch_token_price(session, token, semaphore, task_id + _id)
-                         for _id, token in enumerate(_tokens)]
-                await asyncio.gather(*tasks)
-                task_id += len(_tokens)
+    while True:  # Run indefinitely
+        if semaphore.locked():
+            await asyncio.sleep(2)
+        async with semaphore:
+            tasks = [fetch_token_price(token, semaphore, task_id + _id)
+                     for _id, token in enumerate(_tokens)]
+            await asyncio.gather(*tasks)
+            task_id += len(_tokens)
 
 
 app = FastAPI()
